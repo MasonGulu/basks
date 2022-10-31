4fa101128237bce69eeb6c9b7a15c005c8a230af
Mason Gulu <mason.gulu@gmail.com>
2022-10-31 19:58:28 -0400

    rename

--- install.lua
+++ install.lua
@@ -6,13 +6,19 @@
 -- abstractInvLib https://raw.githubusercontent.com/MasonGulu/msks/main/abstractInvLib.lua
 
 print("Installing basalt..")
-shell.run("wget run https://basalt.madefor.cc/install.lua packed basalt.lua")
+shell.run("wget run https://basalt.madefor.cc/install.lua packed basalt.lua dev")
 
-print("Installing KTWSL..")
+print("Downloading KTWSL..")
 shell.run("wget https://raw.githubusercontent.com/MasonGulu/msks/main/ktwsl.lua ktwsl.lua")
 
-print("Installing Redrun..")
+print("Downloading Redrun..")
 shell.run("wget https://gist.githubusercontent.com/MCJack123/473475f07b980d57dd2bd818026c97e8/raw/139df815254818dc6dc3c7f6ca4a3784f6e0b8f4/redrun.lua redrun.lua")
 
-print("Installing abstractInvLib..")
+print("Downloading abstractInvLib..")
 shell.run("wget https://raw.githubusercontent.com/MasonGulu/msks/main/abstractInvLib.lua abstractInvLib.lua")
+
+print("Downloading basks")
+shell.run("wget https://raw.githubusercontent.com/MasonGulu/BASKS/master/basks.lua basks.lua")
+
+print("Downloading config")
+shell.run("wget https://raw.githubusercontent.com/MasonGulu/BASKS/master/basksconfig.lua config.lua")
