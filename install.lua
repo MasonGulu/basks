e044d083738ca4e55ee95ca507384ebd4ce5d58e
MasonGulu <70228812+MasonGulu@users.noreply.github.com>
2022-12-20 14:23:14 -0500

    Update install.lua

--- install.lua
+++ install.lua
@@ -15,7 +15,7 @@ print("Downloading Redrun..")
 shell.run("wget https://gist.githubusercontent.com/MCJack123/473475f07b980d57dd2bd818026c97e8/raw/139df815254818dc6dc3c7f6ca4a3784f6e0b8f4/redrun.lua redrun.lua")
 
 print("Downloading abstractInvLib..")
-shell.run("wget https://gist.githubusercontent.com/MasonGulu/57ef0f52a93304a17a9eaea21f431de6/raw/0e4bba991532b019873f659f09f09f23a0493b4d/abstractInvLib.lua abstractInvLib.lua")
+shell.run("wget https://gist.githubusercontent.com/MasonGulu/57ef0f52a93304a17a9eaea21f431de6/raw/ce7efb44ce0ad1f5ff668396370b15af500b63df/abstractInvLib.lua abstractInvLib.lua")
 
 print("Downloading msgks")
 shell.run("wget https://raw.githubusercontent.com/MasonGulu/basks/master/msgks.lua msgks.lua")
